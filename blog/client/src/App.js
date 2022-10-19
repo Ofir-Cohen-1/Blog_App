f8cc0f3f6c5f03d66553193fa83dcb0e84677152
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-10-19 17:26:02 +0300

    Added initial Navbar and Footer to router

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,10 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Single from "./pages/Single";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/post/:id", element: <Single /> },
+      { path: "/write", element: <Write /> },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-   Hello World!
+      <RouterProvider router={router} />
     </div>
   );
 }
