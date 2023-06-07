8ef7fe0853da5a354332749617aa0e1cad56898b
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2023-06-07 12:37:01 +0300

    Updated Navbar

--- blog/client/src/components/Navbar.jsx
+++ blog/client/src/components/Navbar.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../img/ofirBlog.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='container'>
+        <div className='logo'>
+          <img src={Logo} alt='' />
+        </div>
+        <div className='links'>
+          <Link className='link' to='/?cat=tech'>
+            {' '}
+            <h6>Tech</h6>
+          </Link>
+          <Link className='link' to='/?cat=design'>
+            {' '}
+            <h6>Design</h6>
+          </Link>
+          <Link className='link' to='/?cat=food'>
+            {' '}
+            <h6>Food</h6>
+          </Link>
+          <span>Ofir</span>
+          <span>Logout</span>
+          <span className='write'>
+            {' '}
+            <Link className='link' to='/write'>
+              Write
+            </Link>{' '}
+          </span>
+        </div>
+      </div>
+    </div>
   )
 }
 
