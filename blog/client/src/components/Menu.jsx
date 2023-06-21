6134191b4124cfdf6679b0ba3dd76027c486e2f8
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2023-06-21 20:23:22 +0300

    Updated Menu component

--- blog/client/src/components/Menu.jsx
+++ blog/client/src/components/Menu.jsx
@@ -1,32 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Menu = () => {
-  const posts = [
-    {
-      id: 1,
-      title: 'lorem1',
-      desc: 'lorem',
-      img: 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg',
-    },
-    {
-      id: 2,
-      title: 'lorem2',
-      desc: 'lorem',
-      img: 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg',
-    },
-    {
-      id: 3,
-      title: 'lorem3',
-      desc: 'lorem',
-      img: 'https://ychef.files.bbci.co.uk/1600x900/p0dqcyrk.webp',
-    },
-    {
-      id: 4,
-      title: 'lorem4',
-      desc: 'lorem',
-      img: 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg',
-    },
-  ]
+const Menu = ({ cat }) => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/?cat=${cat}`)
+        setPosts(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchData()
+  }, [cat])
+
+  // const posts = [
+  //   {
+  //     id: 1,
+  //     title: 'lorem1',
+  //     desc: 'lorem',
+  //     img: 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'lorem2',
+  //     desc: 'lorem',
+  //     img: 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg',
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'lorem3',
+  //     desc: 'lorem',
+  //     img: 'https://ychef.files.bbci.co.uk/1600x900/p0dqcyrk.webp',
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'lorem4',
+  //     desc: 'lorem',
+  //     img: 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg',
+  //   },
+  // ]
 
   return (
     <div className='menu'>
--- blog/client/src/pages/Single.jsx
+++ blog/client/src/pages/Single.jsx
@@ -1,58 +1,84 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import Edit from '../img/edit.png'
 import Delete from '../img/delete.png'
 import { Tooltip } from 'react-tooltip'
 import Menu from '../components/Menu'
+import axios from 'axios'
+import moment from 'moment'
+import { useContext } from 'react'
+import { AuthContext } from '../context/authContext'
 
 const Single = () => {
+  const [post, setPost] = useState({})
+
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const postId = location.pathname.split('/')[2]
+
+  const { currentUser } = useContext(AuthContext)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`)
+        setPost(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchData()
+  }, [postId])
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${postId}`)
+      navigate('/')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div className='single'>
       <div className='content'>
-        <img
-          src='https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg'
-          alt=''
-        />
+        <img src={post?.img} alt='' />
         <div className='user'>
-          <img
-            className='userimg'
-            src='https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg'
-            alt='user'
-          />
+          {post.userImg && (
+            <img className='userimg' src={post.userImg} alt='user' />
+          )}
           <div className='info'>
-            <span>Ofir</span>
-            <p>posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>posted {moment(post.date).fromNow()}</p>
           </div>
-          <div className='edit'>
-            <Tooltip id='my-tooltip' />
-            <Link
-              to={'/write?edit=2'}
-              data-tooltip-id='my-tooltip'
-              data-tooltip-content='Edit'
-              data-tooltip-place='top'
-            >
-              <img src={Edit} alt='Edit' />
-            </Link>
+          {currentUser.username === post.username && (
+            <div className='edit'>
+              <Tooltip id='my-tooltip' />
+              <Link
+                to={'/write?edit=2'}
+                data-tooltip-id='my-tooltip'
+                data-tooltip-content='Edit'
+                data-tooltip-place='top'
+              >
+                <img src={Edit} alt='Edit' />
+              </Link>
 
-            <Link
-              to={'/write?edit=2'}
-              data-tooltip-id='my-tooltip'
-              data-tooltip-content='Delete'
-              data-tooltip-place='top'
-            >
-              <img src={Delete} alt='Delete' />
-            </Link>
-          </div>
+              <Link
+                to={'/write?edit=2'}
+                data-tooltip-id='my-tooltip'
+                data-tooltip-content='Delete'
+                data-tooltip-place='top'
+              >
+                <img onClick={handleDelete} src={Delete} alt='Delete' />
+              </Link>
+            </div>
+          )}
         </div>
-        <h1>lorem ipsum dolor sit amet, consectetur adip</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, at
-          quaerat. Incidunt nisi enim laborum unde hic reprehenderit ipsam?
-          Exercitationem quaerat harum asperiores optio repudiandae nihil sit
-          maiores molestias commodi!
-        </p>
+        <h1>{post.title}</h1>
+        {post.desc}
       </div>
-      <Menu />
+      <Menu cat={post.cat} />
     </div>
   )
 }
