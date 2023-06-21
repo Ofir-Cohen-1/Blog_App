53f7a36c30d6e33eb69707fd1fed3fb5fda31397
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2023-06-21 10:29:08 +0300

    Updated Home page

--- blog/client/src/pages/Home.jsx
+++ blog/client/src/pages/Home.jsx
@@ -1,33 +1,49 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 const Home = () => {
-  const posts = [
-    {
-      id: 1,
-      title: 'lorem1',
-      desc: 'lorem',
-      img: 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg',
-    },
-    {
-      id: 2,
-      title: 'lorem2',
-      desc: 'lorem',
-      img: 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg',
-    },
-    {
-      id: 3,
-      title: 'lorem3',
-      desc: 'lorem',
-      img: 'https://ychef.files.bbci.co.uk/1600x900/p0dqcyrk.webp',
-    },
-    {
-      id: 4,
-      title: 'lorem4',
-      desc: 'lorem',
-      img: 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg',
-    },
-  ]
+  const [posts, setPosts] = useState([])
+
+  const cat = useLocation().search
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts${cat}`)
+        setPosts(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchData()
+  }, [cat])
+
+  // {
+  //   id: 1,
+  //   title: 'lorem1',
+  //   desc: 'lorem',
+  //   img: 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg',
+  // },
+  // {
+  //   id: 2,
+  //   title: 'lorem2',
+  //   desc: 'lorem',
+  //   img: 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg',
+  // },
+  // {
+  //   id: 3,
+  //   title: 'lorem3',
+  //   desc: 'lorem',
+  //   img: 'https://ychef.files.bbci.co.uk/1600x900/p0dqcyrk.webp',
+  // },
+  // {
+  //   id: 4,
+  //   title: 'lorem4',
+  //   desc: 'lorem',
+  //   img: 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg',
+  // },
+
   return (
     <div className='home'>
       <div className='posts'>
